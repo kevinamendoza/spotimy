91d92fd00752b33226a081320a542dc8ce121d61
Kevin Mendoza Tudares <kevinamendoza@users.noreply.github.com>
2020-06-04 13:00:34 -0500

    Updates

---
+++ precache-manifest.f3d84ce29648ab3320a8aa12eff6fedb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ebb1267de1254c46bf7dfb2f29b6db7",
+    "url": "/spotimy/index.html"
+  },
+  {
+    "revision": "80a392574f6947a4fba6",
+    "url": "/spotimy/static/css/main.c15dab1e.chunk.css"
+  },
+  {
+    "revision": "b34f56c6203a29472b34",
+    "url": "/spotimy/static/js/2.27dccc29.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/spotimy/static/js/2.27dccc29.chunk.js.LICENSE"
+  },
+  {
+    "revision": "80a392574f6947a4fba6",
+    "url": "/spotimy/static/js/main.bf5e2f0b.chunk.js"
+  },
+  {
+    "revision": "80cd93c95b535eb624e1",
+    "url": "/spotimy/static/js/runtime-main.931533a5.js"
+  }
+]);
